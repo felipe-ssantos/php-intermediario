a3470a0f5141b498f81c583488f91f05fdc601c9
https://github.com/felipe-ssantos <felipesilveirasantos.contato@gmail.com>
2023-09-26 21:48:35 -0300

    função date

--- php_05/index.php
+++ php_05/index.php
@@ -1,21 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Date</title>
+</head>
+
+<body>
+	<?php
 	/*
-		* Trabalhando com datas
-	*/
+	 * Trabalhando com datas
+	 */	
 
+	 date_default_timezone_set('America/Sao_Paulo');	 
+	 
+	 $data = date('d/m/Y H:i:s');
+	 echo $data;
+	 
+	 
 	/*
-	date_default_timezone_set('America/Sao_Paulo');	
+	   $titulo_site = 'Titulo do meu site';
+	   include('header.php');
+	   */
+	?>
 
-	$data = date('d/m/Y H:i:s',time()+(60*10));
- 
-	echo $data;
-	*/
-	$titulo_site = 'Titulo do meu site';
-	include('header.php');
-?>
+	<h1>Meu conteudo da home!</h1>
 
-<h1>Meu conteudo da home!</h1>
-
-<?php
+	<?php
 	include('footer.php');
-?>
+	?>
+</body>
+
+</html>
